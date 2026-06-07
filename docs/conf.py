da6cdfc8dc4d4dc6186ae1dc95ceebe2c7812d63
aouwalitshikkha <aouwalitshikkha@gmail.com>
2026-06-07 14:53:58 +0600

    Update docs conf.py release to 0.1.10

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "llms-generator"
 copyright = "2026, aouwalitshikkha"
 author = "aouwalitshikkha"
-release = "0.1.5"
+release = "0.1.10"
 
 extensions = [
     "myst_parser",
